a207737cd661d648d7267267f23cd14f02eb3a50
Oleh Shyian <shiyan4242@gmail.com>
2024-11-28 11:20:42 +0100

    s

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -16,7 +16,7 @@ class ErrorBoundary extends Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something wrong...</h1>;
+      return <h1>Something</h1>;
     }
 
     return this.props.children;
